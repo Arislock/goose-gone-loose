43a3e98f9b5f1bd0f9da50cb95445c3cc85dfa39
elinzhang507 <elinzhang507@gmail.com>
2026-09-20 07:47:07 -0400

    delete unused signals

--- SignalBus.gd
+++ SignalBus.gd
@@ -1,11 +1,8 @@
 extends Node
 
-# global event signals for the whole project
-signal goose_jumped
-signal goose_ducked
+# global event signals 
+signal goose_jumped(is_shake: bool)
 signal goose_charged
-signal goose_started_flying
-signal goose_stopped_flying
 signal game_over
 signal obstacle_passed
 signal score_updated(new_score: int)
