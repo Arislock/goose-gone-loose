d23f2f1333b8f580d8f281c55d614723dca7a4bc
ditripy <robel.trinitymae@gmail.com>
2026-09-20 02:03:19 -0400

    fix fly

--- SignalBus.gd
+++ SignalBus.gd
@@ -1,8 +1,11 @@
-# Inside res://SignalBus.gd
 extends Node
 
-signal goose_jumped(is_shake: bool)
+# The central event transmitters for the whole project
+signal goose_jumped
+signal goose_ducked
 signal goose_charged
+signal goose_started_flying
+signal goose_stopped_flying
 signal game_over
 signal obstacle_passed
 signal score_updated(new_score: int)
